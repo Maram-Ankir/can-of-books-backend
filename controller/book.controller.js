8d2d6cfa0dec1759ada06ebb68c0e61db2a13ee1
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-22 15:31:52 +0300

    improve backend functionality,add update,delete,creat functions

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -1,19 +1,79 @@
 'use strict';
 
-const {userModel} = require('../models/user.model');
+const { userModel } = require('../models/user.model');
 
 const getbooks = (request, response) => {
 
     const { email } = request.query;
 
-    userModel.find({ email: email }, (error, user) => {
+    userModel.findone({ email: email }, (error, userData) => {
         if (error) {
             response.send(error)
         } else {
-            response.json(user)
+            response.json(userData)
         }
     });
 }
 
-module.exports = getbooks;
+
+///////////////////////////////////////////////////////
+const createBook = (request, response) => {
+
+    const { userEmail, bookName } = request.body;
+
+    userModel.findone({ email: userEmail }, (error, userData) => {
+        if (error) {
+            response.send(error)
+        } else {
+            userData.books.push({ name: bookName });
+            userData.save();
+            response.json(userData);
+        }
+    });
+}
+
+
+/////////////////////////////////////////////////
+const updateBook = (request, response) => {
+
+    const { userEmail, bookName } = request.body;
+    const bookIndex = request.params.book_idx;
+
+    userModel.findone({ email: userEmail }, (error, userData) => {
+        if (error) {
+            response.send(error)
+        } else {
+            userData.books.splice(bookIndex, 1, { name: bookName });
+            userData.save();
+            response.json(userData);
+        }
+    });
+}
+
+
+//////////////////////////////////////////////////
+const deleteBook = (request, response) => {
+
+    const { userEmail } = request.query;
+    const bookIndex = request.params.book_idx;
+
+
+    userModel.findone({ email: userEmail }, (error, userData) => {
+        if (error) {
+            response.send(error)
+        } else {
+            userData.cats.splice(bookIndex, 1);
+            userData.save();
+            response.json(userData)
+        }
+    });
+}
+
+
+
+module.exports = {
+    getbooks, createBook,
+    updateBook,
+    deleteBook
+};
 
--- server.js
+++ server.js
@@ -1,20 +1,39 @@
-const express = require('express') 
-const app = express() 
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
+
 require('dotenv').config();
-const {seedUserData} = require('./models/user.model')
-const getbooks = require('./controller/book.controller');
-const cors = require('cors'); 
-app.use(cors())
+
+const { seedUserData } = require('./models/user.model');
+
+const { getbooks,
+    createBook,
+    updateBook,
+    deleteBook } = require('./controller/book.controller');
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
 
 const PORT = process.env.PORT;
+
 mongoose.connect('mongodb://localhost:27017/myFavoriteBooks',
-    { useNewUrlParser: true, useUnifiedTopology: true }
-);
+    { useNewUrlParser: true, useUnifiedTopology: true });
 
-seedUserData(); 
+seedUserData();
 
-app.get('/books', getbooks)
+app.get('/books', getbooks);
+
+app.post('/book', createBook);
+
+app.delete('/book/:book_idx', deleteBook)
+
+app.put('/book/:book_idx', updateBook);
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
